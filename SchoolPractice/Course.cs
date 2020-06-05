b399d384b701db040dc53b3e01f94510100b88a9
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-06-05 10:44:12 -0400

    updated course class

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        public string CourseName { get; set; }
+        public int CourseCredit { get; set; }
+
+        private List<string> roster = new List<string>();
+
+        public Course(string courseName, int courseCredit)
+        {
+            CourseName = courseName;
+            CourseCredit = courseCredit;
+        }
+
+        public void addStudent(Student student)
+        {
+            roster.Add(student.Name);
+            //Console.WriteLine(student.Name);
+        }
+
+        public void printRoster()
+        {
+            foreach(string item in roster)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,24 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            // Building new student without constructor
+            //Student andrewM = new Student();
+            //andrewM.Name = "Andrew";
+            //andrewM.StudentId = 11111;
+            //andrewM.NumberOfCredits = 1;
+            //andrewM.Gpa = 4.0;
+
+            // Building new objects with constructor
+            Student andrewM = new Student("Andrew", 11111, 1, 4.0);
+            Course lc101 = new Course("LC 101", 1);
+
+            // demonstrate methods inside Course using Student
+            lc101.addStudent(andrewM);
+
+            // demonstrate that both this and prior method function as expected
+            lc101.printRoster();
+
         }
     }
 }
